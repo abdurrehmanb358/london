506751d8acaad2eef17a23e8c29496d7cbebc652
Abdurrehman <abdurrehmanb358@gmail.com>
2023-08-22 12:24:36 +0500

    navbarset

--- resources/views/layouts/mainbar.blade.php
+++ resources/views/layouts/mainbar.blade.php
@@ -2,16 +2,9 @@
     <nav class="mainNav">
         <ul>
             <li> <a href="{{route('homepage')}}">Home</a></li>
-            <li> <a href="{{route('hotel_listing')}}">Hotels</a></li>
-            <li> <a href="{{route('Umrah_listing')}}">Umrah</a></li>
-            <li> <a href="designers.php">DESIGNERS</a></li>
-            <li> <a href="projects.php">PROJECTS</a></li>
-<<<<<<< HEAD
-
-=======
+           <li> <a href="{{route('Umrah_listing')}}">Umrah</a></li>
             <li> <a href="{{route('hotel_listing')}}">Hotels</a></li>
             <li> <a href="{{route('flight_listing')}}">Flights</a></li>
->>>>>>> 7215c0827cb5200f6df0677cc12dbdda636726ed
             <li> <a class="scrollNav" href="#pricingSec">PRICING </a></li>
             <li> <a class="scrollNav" href="#pricingSec">GET IN TOUCH </a></li>
         </ul>
