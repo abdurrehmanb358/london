65028bd74f577077053952182637ec774cbdeadc
Abdurrehman <abdurrehmanb358@gmail.com>
2023-09-28 12:18:28 +0500

    changes in icon

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -110,6 +110,7 @@ id="sidenav-main">
 
           <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
      
+            <i class="fas fa-hotel"></i>
 
           </div>
 
@@ -123,7 +124,8 @@ id="sidenav-main">
         <a class="nav-link text-white " href="{{route('back-panel.insurance')}}">
 
           <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-          
+            <i class="fas fa-hotel"></i>
+
           </div>
 
           <span class="nav-link-text ms-1">Insurance Inquiry</span>
